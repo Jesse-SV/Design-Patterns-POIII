9588b547994696e47ebfbbc437f19bf97cce6bb6
Jessé Santana Veloso <115315621+Jesse-SV@users.noreply.github.com>
2024-04-25 11:32:07 -0300

    Delete src directory

--- src/transport/vehicles/interfaces/IVehicle.ts
+++
@@ -1,6 +0,0 @@
-//Export é obrigatório e serve para o import funcionar
-//Default obriga a ter só uma interface
-export default interface IVehicle{
-    startRoute() : void;
-    getCargo() : void;
-}
