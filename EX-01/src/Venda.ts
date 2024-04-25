c045bc2d80d53c5f318f4000efe3daf47589ce37
Jessé Santana Veloso <115315621+Jesse-SV@users.noreply.github.com>
2024-04-25 13:30:29 -0300

    Update Venda.ts

--- EX-01/src/Venda.ts
+++ EX-01/src/Venda.ts
@@ -2,10 +2,37 @@ import { Cliente } from "./Cliente";
 import { Produto } from "./Produto";
 
 export class Venda{
-    codigo : number;
-    data : number;
-    cliente : Cliente;
-    produtos : Produto[];
+    private _codigo : number;
+    private _data : number;
+    private _cliente : Cliente;
+    private _produtos : Produto[];
+    
+    //Setters
+    public set codigo(value: number) {
+        this._codigo = value;
+    }
+    public set data(value: number) {
+        this._data = value;
+    }
+    public set cliente(value: Cliente) {
+        this._cliente = value;
+    }
+    public set produtos(value: Produto[]) {
+        this._produtos = value;
+    }
+    //Getters
+    public get codigo() {
+        return this._codigo;
+    }
+    public get data() {
+        return this._data;
+    }
+    public get cliente() {
+        return this._cliente;
+    }
+    public set produtos() {
+        return this._produtos;
+    }
 
     public calcularTotal(): number{
         let i: number;
